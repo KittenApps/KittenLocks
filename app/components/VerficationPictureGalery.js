34bd3566ed130b41e5c7fbb320714ea78c016859
Silizia <66546362+KittenApps@users.noreply.github.com>
2021-12-01 14:00:11 +0100

    VerificationPictureGallery: add code

--- app/components/VerficationPictureGalery.js
+++ app/components/VerficationPictureGalery.js
@@ -8,7 +8,7 @@ export default function VerficationPictureGalery({ data }){
   useEffect(() => {
     if (data){
       Promise.all(data.map(e => fetch(`https://api.chaster.app/files/${e.imageKey}`).then(d => d.json())
-                                .then(d => ({ src: d.url, title: new Date(e.submittedAt) }))))
+                                .then(d => ({ src: d.url, code: e.verificationCode, title: new Date(e.submittedAt) }))))
       .then(d => d.sort((a, b) => a.title - b.title)).then(d => setPics(d));
     }
   }, [data]);
@@ -32,7 +32,7 @@ export default function VerficationPictureGalery({ data }){
         {pics.map((img, i) => (
           <ImageListItem style={{ minHeight: 48, cursor: 'pointer' }} onClick={handleClick(img)} key={img.title.toString()}>
             <img src={img.src} alt={img.title.toLocaleString()}/>
-            <ImageListItemBar title={`${img.title.toLocaleString()} (#${i + 1})`}/>
+            <ImageListItemBar title={`${img.title.toLocaleString()} (${img.code}) #${i + 1}`}/>
           </ImageListItem>
         ))}
       </Masonry>
@@ -40,7 +40,7 @@ export default function VerficationPictureGalery({ data }){
         <div>
           <img src={selected?.src} alt={selected?.title.toLocaleString()} style={{ maxWidth: '100%', maxHeight: '100%', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', boxShadow: '0px 11px 15px -7px rgba(0,0,0,0.2),0px 24px 38px 3px rgba(0,0,0,0.14),0px 9px 46px 8px rgba(0,0,0,0.12)', outline: 'none' }}/>
           <h3 style={{ position: 'absolute', bottom: -52, width: '100%', textAlign: 'center' }}>
-            {selected?.title.toLocaleString()}
+            {`${selected?.title.toLocaleString()} (${selected?.code})`}
             { navigator.share && <IconButton size="small" aria-label="share" onClick={handleShare(selected?.src)} sx={{ pt: 0 }} component="span"><IosShareIcon/></IconButton>}
           </h3>
         </div>
