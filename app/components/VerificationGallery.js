28160256ae9cae9ee2a4ff4ec96936b40b17a358
Silizia <66546362+KittenApps@users.noreply.github.com>
2021-12-27 21:42:19 +0100

    update image viewer to forked version with touch support

--- app/components/VerificationGallery.js
+++ app/components/VerificationGallery.js
@@ -2,8 +2,7 @@ import { memo, useCallback, useMemo, useState } from 'react';
 import { ImageListItem, ImageListItemBar, Typography } from '@mui/material';
 import { Masonry } from '@mui/lab';
 import { IosShare } from '@mui/icons-material';
-import ReactViewer from 'react-viewer';
-import { ClassNames } from '@emotion/react';
+import ReactViewer from 'react-viewer'; // eslint-disable-line import/no-unresolved
 
 const VerificationPicture = memo(({ img, setSelected }) => {
   const handleClick = useCallback(() => setSelected(img.i), [img, setSelected]);
@@ -33,43 +32,20 @@ function VerificationPictureGallery({ history }){
       <Masonry columns={{ xs: 2, sm: 3, lg: 4 }} spacing={1}>
         { imgs.map(img => <VerificationPicture key={img.key} img={img} setSelected={setSelected}/>)}
       </Masonry>
-      <ClassNames>
-        {({ css }) => (
-          <ReactViewer
-            visible={selected >= 0}
-            onClose={handleClose}
-            images={imgs}
-            activeIndex={selected}
-            zIndex={1201}
-            onMaskClick={handleClose}
-            downloadable
-            noImgDetails
-            zoomSpeed={0.1}
-            downloadInNewWindow
-            showTotal={false}
-            customToolbar={extendToolbar}
-            className={css`
-              & .react-viewer-mask {
-                background-color: rgba(0, 0, 0, 0.75);
-              }
-              & .react-viewer-attribute {
-                color: #ffffff;
-                font-size: 20px;
-                font-weight: 600;
-              }
-              & .react-viewer-toolbar {
-                height: 36px;
-              }
-              & .react-viewer-toolbar li {
-                width: 36px;
-                height: 36px;
-                border-radius: 36px;
-                line-height: 36px;
-              }
-            `}
-          />
-        )}
-      </ClassNames>
+      <ReactViewer
+        visible={selected >= 0}
+        onClose={handleClose}
+        images={imgs}
+        activeIndex={selected}
+        zIndex={1201}
+        onMaskClick={handleClose}
+        downloadable
+        noImgDetails
+        zoomSpeed={0.1}
+        downloadInNewWindow
+        showTotal={false}
+        customToolbar={extendToolbar}
+      />
     </>
   );
 }
