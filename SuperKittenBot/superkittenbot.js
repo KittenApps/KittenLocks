d3726f9bdb5a5a9bdbd80065bdd1cc42a7b05989
Silizia <66546362+KittenApps@users.noreply.github.com>
2022-02-14 01:12:10 +0100

    upgrade deps

--- SuperKittenBot/superkittenbot.js
+++ SuperKittenBot/superkittenbot.js
@@ -1,29 +1,27 @@
-const nacl = require('tweetnacl');
-const http = require('http');
+/* eslint-disable unicorn/prefer-module, unicorn/filename-case */
+/* eslint-env node */
+const { sign } = require('tweetnacl');
+const https = require('https');
 
-exports.handler = async function(event){
-  const isVerified = nacl.sign.detached.verify(
-    Buffer.from(event.headers['x-signature-timestamp'] + event.body),
-    Buffer.from(event.headers['x-signature-ed25519'], 'hex'),
-    Buffer.from(process.env.DISCORD_BOT_PUBLIC_KEY, 'hex')
-  );
-  if (!isVerified) return {
-    statusCode: 401,
-    body: 'invalid request signature'
-  };
+exports.handler = async event => { // eslint-disable-line require-await
+  const isVerified = sign.detached.verify(Buffer.from(event.headers['x-signature-timestamp'] + event.body),
+                                          Buffer.from(event.headers['x-signature-ed25519'], 'hex'),
+                                          Buffer.from(process.env.DISCORD_BOT_PUBLIC_KEY, 'hex'));
+  if (!isVerified) return { statusCode: 401, body: 'invalid request signature' };
 
   const json = JSON.parse(event.body);
-  if (json.type === 1) return {
-    statusCode: 200,
-    body: JSON.stringify({ type: 1 })
-  };
+  console.log(json);
 
-  const req = http.request('https://data.mongodb-api.com/app/kittenlocks-gcfgb/endpoint/superkittenbot',
-                           { method: 'POST', headers: { 'Content-Type': 'application/json' } });
+  if (json.type === 1) return { statusCode: 200, headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ type: 1 }) };
+
+  const req = https.request('https://data.mongodb-api.com/app/kittenlocks-gcfgb/endpoint/superkittenbot',
+                            { method: 'POST', headers: { 'Content-Type': 'application/json' } });
   req.write(event.body);
   req.end();
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ type: 5 })
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ type: 4, data: { content: 'SuperKittenBot pokes back!' } })
   };
 };
