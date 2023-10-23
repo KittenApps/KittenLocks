15bc34cf49c0e5d55cb5ae60807497a2806d8030
Silizia <66546362+KittenApps@users.noreply.github.com>
2023-10-23 11:33:19 +0200

    fix Discord Widget

--- app/views/Discord.jsx
+++ app/views/Discord.jsx
@@ -1,13 +1,14 @@
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 import { Paper } from '@mui/material';
 import { useRealmApp } from '../RealmApp';
 
-function Discord({ username }){
+function Discord(){
   const app = useRealmApp();
+  const params = useMemo(() => (app.currentUser?.customData ? `?${new URLSearchParams({ username: app.currentUser.customData.username, avatar: app.currentUser.customData.avatarUrl })}` : ''), [app.currentUser?.customData]);
 
   return (
     <Paper elevation={6} sx={{ position: 'absolute', backgroundColor: '#1b192a', top: { xs: 56, md: 80 }, left: { xs: 0, md: 256 }, right: { xs: 0, md: 16 }, bottom: { xs: 0, md: 16 }, p: 2 }} >
-      <iframe src={`https://e.widgetbot.io/channels/879777377541033984/879777377968869465${username ? `?username=${encodeURIComponent(app.currentUser?.customData?.username)}` : ''}`} title="Discord" width="100%" height="100%" frameBorder="0"/>
+      <iframe src={`https://e.widgetbot.io/channels/879777377541033984/879777377968869465${params}`} title="Discord Wifget" style={{ width: '100%', height: '100%', border: 'none' }} allow="clipboard-write; fullscreen"/>
     </Paper>
   );
 }
